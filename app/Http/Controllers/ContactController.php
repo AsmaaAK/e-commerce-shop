dc71523f6d7729e1e7105e844bba4e77e0ac8665
Asmaa AK <asoomak97@gmail.com>
2025-09-06 08:02:05 -0700

    feat: Implement form submission and validation logic

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class ContactController extends Controller
 {
-    //
+    public function submit(Request $request)
+    {
+        $validated = $request->validate([
+            'name'    => ['required','min:3'],
+            'email'   => ['required','email'],
+            'message' => ['required','max:500'],
+        ]);
+
+
+        return back()->with('success', 'Your message has been sent successfully!');
+    }
 }
