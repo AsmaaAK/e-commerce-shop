02adee851604ab1a147e3ce7d7984fb927efb221
Asmaa AK <asoomak97@gmail.com>
2025-10-03 14:42:16 -0700

    feat: Apply middleware for route protection

--- routes/web.php
+++ routes/web.php
@@ -2,19 +2,17 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::middleware(['auth', 'can:access-admin-panel'])->prefix('admin')->group(function () {
+    Route::get('/dashboard', [AdminController::class, 'dashboard']);
+    Route::get('/products', [AdminController::class, 'products']);
+    Route::get('/categories', [AdminController::class, 'categories']);
 });
+
 
 require __DIR__.'/auth.php';
