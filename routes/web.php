a65813b59382f90b09310d73af8155ba5daad83c
Asmaa AK <asoomak97@gmail.com>
2025-10-03 14:31:36 -0700

    feat: Install and configure Laravel Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,49 +1,20 @@
 <?php
 
-use App\Http\Controllers\ShopController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\StoreController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\ContactController;
-use App\Mail\WelcomeEmail;
-use App\Notifications\NewOrderNotification;
-use Illuminate\Support\Facades\Mail;
-use App\Http\Controllers\AdminController;
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/shop', [ShopController::class, 'index'])->name('shop.index');
-Route::get('/products', [StoreController::class, 'index'])->name('products.index');
-Route::get('/product/{id}', [StoreController::class, 'show'])->name('product.show');
-Route::get('/cart', [StoreController::class, 'cart'])->name('cart');
-Route::get('/about', [StoreController::class, 'about'])->name('about');
-Route::get('/contact', [StoreController::class, 'contact'])->name('contact');
-Route::get('/shopcart', [ShopController::class, 'cart'])->name('shopcart.cart');
-Route::get('/shop/checkout', [ShopController::class, 'checkout'])->name('shop.checkout');
-Route::get('/product/{id?}', [ShopController::class, 'details'])->name('product.details');
-// Route::get('/shop/product-details', [ShopController::class, 'details'])->name('product.details');
-Route::get('/admin',[ProductController::class,'index'])->name('products.index');
-Route::get('/products/create',[ProductController::class,'create'])->name('products.create');
-Route::post('/products/stot',[ProductController::class,'store'])->name('products.store');
-Route::get('/products/show/{id}',[ProductController::class,'show'])->name('products.show');
-Route::get('/products/{id?}/edit',[ProductController::class,'edit'])->name('products.edit');
-Route::put('/products/{id}',[ProductController::class,'update'])->name('products.update');
-Route::delete('/products/delete/{id}',[ProductController::class,'destroy'])->name('products.destroy');
-Route::post('/contact', [ContactController::class, 'submit'])->name('contact.submit');
 
-Route::get('/test-mail', function () {
-    Mail::to('test@example.com')->send(new WelcomeEmail());
-    return "Welcome Email Sent!";
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/test-notification', function () {
-    $user = \App\Models\User::first();
-    $user->notify(new NewOrderNotification());
-    return "Notification Sent!";
-});
-Route::prefix('admin')->group(function () {
-    Route::get('/dashboard', [AdminController::class, 'dashboard']);
-    Route::get('/products', [AdminController::class, 'products']);
-    Route::get('/categories', [AdminController::class, 'categories']);
-});
+require __DIR__.'/auth.php';
